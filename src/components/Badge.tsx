aebe95481897438713d6cfc800260b8e74071684
yyyyaaa <phatghaa@gmail.com>
2023-12-16 03:08:55 +0700

    fix: stuff

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -3,7 +3,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import clsx from "clsx";
 
 const badgeVariants = cva(
-  "inline-flex min-h-[32px] items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  "inline-flex min-h-[32px]  items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
   {
     variants: {
       variant: {
